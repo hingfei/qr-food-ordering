d8d2b4e0e026eb5adc03bd05b6b2d1f9a7c89fcc
khooihzhz <khooihongzhe@gmail.com>
2021-09-30 16:22:51 +0800

    Nav Bar Fix

--- src/views/TableNumber/TableNumberNavBar.js
+++ src/views/TableNumber/TableNumberNavBar.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import {Navbar} from "react-bootstrap";
 
 function TableNumberNavBar(props) {
     return (
-        <div>
-            NavBar
-        </div>
+        <>
+            <Navbar bg="light">
+                    <Navbar.Brand href="#home">
+                        <img
+                            alt=""
+                            src="./images/logo.svg"
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top"
+                        />{' '}
+                        Great Food
+                    </Navbar.Brand>
+            </Navbar>
+        </>
     );
 }
 
