cbcdc950d53c62bf63dd724f29026aec34ecd32f
Guofoong Ng <guofoong1004@gmail.com>
2021-09-30 16:29:32 +0800

    RestaurantName

--- src/views/TableNumber/TableNumberRestaurantName.js
+++ src/views/TableNumber/TableNumberRestaurantName.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import image1 from './images/decklogo.jpg';
+import mystyles from './RestaurantName.css';
 
 function TableNumberRestaurantName(props) {
     return (
-        <div>
-            Restaurant Name
+        <div className = "restaurant">
+            <h1 className="name"> THE DECK RESTAURANT</h1>
+            <img src={image1} alt="" 
+            class="center"
+            height = {150}
+            width = {300} />
         </div>
     );
 }
