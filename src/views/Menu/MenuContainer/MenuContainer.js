ca48717cd97764b551a3b985c1173b7bc65d04df
hingfei <soonghingfei@hotmail.com>
2021-10-01 16:39:25 +0800

    footer

--- src/views/Menu/MenuContainer/MenuContainer.js
+++ src/views/Menu/MenuContainer/MenuContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import MenuRestaurantName from '../MenuRestaurantName';
 import MenuNavBar from "../MenuNavBar";
+import MenuFooter from '../MenuFooter';
 
 function MenuContainer(props) {
     return (
@@ -9,7 +10,7 @@ function MenuContainer(props) {
             <MenuNavBar/>
             <MenuRestaurantName/>
 
-
+            <MenuFooter/>
         </div>
     );
 }
