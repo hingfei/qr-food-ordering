326ec700f5ff1361df9bc35704fbcd23e815e443
hingfei <soonghingfei@hotmail.com>
2021-10-01 17:36:23 +0800

    menu container

--- src/views/Menu/MenuContainer/MenuContainer.js
+++ src/views/Menu/MenuContainer/MenuContainer.js
@@ -4,6 +4,7 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import MenuRestaurantName from '../MenuRestaurantName';
 import MenuNavBar from "../MenuNavBar";
 import MenuFooter from '../MenuFooter';
+import MenuItem from "../MenuItem";
 
 function MenuContainer() {
     return (
@@ -16,7 +17,7 @@ function MenuContainer() {
              </Switch>
             </Router>
             <MenuRestaurantName/>
-
+            <MenuItem/>
             <MenuFooter/>
         </div>
     );
