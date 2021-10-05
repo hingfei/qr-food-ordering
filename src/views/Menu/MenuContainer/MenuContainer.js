14b4eaf9d834f4bc38114c4441db98feec3a9201
Guofoong Ng <guofoong1004@gmail.com>
2021-10-05 14:03:16 +0800

    Escape

--- src/views/Menu/MenuContainer/MenuContainer.js
+++ src/views/Menu/MenuContainer/MenuContainer.js
@@ -6,27 +6,16 @@ import MenuNavBar from "../MenuNavBar";
 import MenuFooter from '../MenuFooter';
 import MenuItem from '../MenuItem';
 
-<<<<<<< HEAD
-function MenuContainer(props) {
-    return (
-        <div>
-            Menu page
-=======
 function MenuContainer() {
     return (
         <div>
             <MenuNavBar/>
->>>>>>> 05341db4f6c3a21d68854438bc24574a46e14c1e
             <Router>
              <MenuSideNav/>
              <Switch>
                  <Route path='/' />
              </Switch>
             </Router>
-<<<<<<< HEAD
-            <MenuNavBar/>
-=======
->>>>>>> 05341db4f6c3a21d68854438bc24574a46e14c1e
             <MenuRestaurantName/>
             <MenuItem/>
             <MenuFooter/>
