4aacf34518d03d78f384f37160bffbc18f72d33f
Guofoong Ng <guofoong1004@gmail.com>
2021-10-08 14:35:45 +0800

    testing

--- src/views/TableNumber/TableNumberRestaurantName.js
+++ src/views/TableNumber/TableNumberRestaurantName.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import image1 from '../images/decklogo.jpg';
 import './TableNumberRestaurantName.css';
+import {Typography} from "@mui/material";
 
 function TableNumberRestaurantName(props) {
     return (
         <div className ="restaurant">
-            <h1 className="name"> THE DECK RESTAURANT</h1>
+            <Typography variant="h3" color = "inherit" align = "center"  className = "name"
+            >
+             THE DECK RESTAURANT    
+            </Typography>
             <img src={image1} alt="" 
             className="center"
             height = {150}
             width = {300} />
+            
         </div>
+         
     );
 }
 
