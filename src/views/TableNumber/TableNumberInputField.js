0875b2003d56cd1007c436267e3b39fe7081b3f6
KS <kahsingc99@gmail.com>
2021-10-08 15:25:46 +0800

    textfield

--- src/views/TableNumber/TableNumberInputField.js
+++ src/views/TableNumber/TableNumberInputField.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import './TableNumberInputField.css';
+import {InputAdornment, TextField} from "@mui/material";
+import ChairAltTwoToneIcon from '@mui/icons-material/ChairAltTwoTone';
 
 function TableNumberInputField(props) {
     return (
         <div>
-            <form>
-                <label>
-                    <p>Please Input Your Table Number:</p>
-                    <input type="text" placeholder="Enter table number" /*value=tableNumber*/ />
-                </label>
-            </form>
+            <TextField id="filled-required" label="Table Number" variant="filled" size="normal" color="secondary" helperText="Fill in your table number" InputProps={{startAdornment:(<InputAdornment position={'start'}><ChairAltTwoToneIcon/></InputAdornment>)}}/>
         </div>
     );
 }
