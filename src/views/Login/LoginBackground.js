c06f8e8f648a4d9f1eac7a8814842148c814277d
KS <kahsingc99@gmail.com>
2021-11-25 16:40:10 +0800

    add login image

--- src/views/Login/LoginBackground.js
+++ src/views/Login/LoginBackground.js
@@ -4,14 +4,19 @@ import AssignmentInd from '@mui/icons-material/AssignmentInd';
 import {Checkbox, FormControlLabel} from "@mui/material";
 import LoginInputField from "./LoginInputField";
 import LoginButton from "./LoginButton";
+import login_img from "../images/menu.jpg";
+import "./LoginBackground.css";
 
 function LoginBackground(){
     return(
-        <Box sx={{color:"#1E7DBC", display:"inline-flex", flexDirection:"column", alignItems:"center"}}>
+        <Box ml={5} mt={7}>
+            <img src={login_img} alt="login" id="login_img"/>
+        <Box ml={40} sx={{color:"#1E7DBC", display:"inline-flex", flexDirection:"column", alignItems:"center"}}>
             <AssignmentInd sx={{fontSize:100}}/>
             <LoginInputField/>
             <FormControlLabel control={<Checkbox/>} label="Remember me"/>
             <LoginButton/>
+        </Box>
         </Box>
     );
 }
