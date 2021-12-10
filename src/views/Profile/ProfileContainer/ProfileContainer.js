128c6b754b2339aa9213c245cba5a2c16355de4d
hingfei <soonghingfei@hotmail.com>
2021-12-10 20:49:04 +0800

    Profile page

--- src/views/Profile/ProfileContainer/ProfileContainer.js
+++ src/views/Profile/ProfileContainer/ProfileContainer.js
@@ -4,21 +4,26 @@ import Footer from "../../../components/Footer";
 import ProfileQrGenerator from "../ProfileQrGenerator";
 import ProfileNavBar from "../ProfileNavBar";
 import ProfileQrDownloadButton from "../ProfileQrDownloadButton";
+import ProfileName from '../ProfileName';
+import {Paper} from '@mui/material';
 
 function ProfileContainer(){
     return (
-        <Box sx={{display: "flex", flexDirection: "column", minHeight:"100vh",backgroundColor:"FBF1E4"}}>
+        <Box sx={{minHeight:"100vh",backgroundColor:"FBF1E4"}}>
             <ProfileNavBar/>
-            <Box sx={{
-                display: "inline-flex",
-                flexDirection:"column",
-                alignItems: "center",
-                minHeight: "70vh",
-                justifyContent: "center"
-            }}>
-                
-                <ProfileQrGenerator/>
-                <ProfileQrDownloadButton/>
+            <Box display="flex">
+                <Paper sx={{minWidth:"30%", minHeight:"70vh", alignSelf:"center"}}>
+                    <ProfileName/>
+                </Paper>
+                <Box display="flex"
+                     minWidth="70%"
+                     flexDirection="column" 
+                     justifyContent="center" 
+                     alignItems="center"
+                >
+                    <ProfileQrGenerator/>
+                    <ProfileQrDownloadButton/>
+                </Box>
             </Box>
             <Footer/>
         </Box>
