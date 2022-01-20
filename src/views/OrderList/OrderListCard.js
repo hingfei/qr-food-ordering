3378c675e5f6d1399bdf8fb8d2394cdbeec73854
hingfei <soonghingfei@hotmail.com>
2022-01-20 19:08:01 +0800

    Order list page done

--- src/views/OrderList/OrderListCard.js
+++ src/views/OrderList/OrderListCard.js
@@ -1,90 +1,135 @@
-import React, {useContext} from "react";
-import { OrderListContext } from "../../App";
+import React, { useEffect, useState} from "react";
 import {Box, Card, CardContent, Typography, CardActions, Button} from '@mui/material';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import OrderListDialog from './OrderListDialog';
+import LoadingSpinner from "../../components/LoadingSpinner";
+import axios from "axios";
 
 function OrderListCard() {
-    const ordersContext = useContext(OrderListContext);
+    const [orders, setOrders] = useState();
+    const [value, setValue] = useState();
+    const [isLoading, setIsLoading] = useState(true);
+    
     let status="";
     let served="";
 
+    const AuthConfig = {
+        headers: {
+            Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+    };
+
+    // Get orders of the restaurant
+    useEffect(() => {
+        if (isLoading) {
+            axios.get('/restaurant/orders', AuthConfig)
+                .then((result) => {
+                    setOrders(result.data);
+                    setIsLoading(false);
+                }).catch((error) => {
+                    console.log(error);
+                    setIsLoading(false);
+                })
+        }
+    }, []) 
+
     // Function to indicate order is done.
-    const handleFinish = (id, served) => {
-        ordersContext.ordersDispatch({type:'served', payload: {id: id, served: served}});
-        console.log("Order finish");
+    const handleFinish = (id) => {
+        // Set to served
+        const data = {
+            "done": true
+        }
+        // Update database
+        axios.put('/orders/'.concat(id), data)
+            .then((result) => {
+                console.log(result);
+                window.location.reload();
+            }).catch((error) => {
+                console.log(error);
+            })
     }
 
-    const extractOrders = (order) => {
-        // Todo: extract order details from str to json.
+    
+    // Extract order details from str to json.
+    const extractOrders = (orders) => {
+        orders = JSON.parse(orders);
+        console.log(orders);
+        return orders;
     }
 
-    return (
-        <Box 
-            display="flex" 
-            flexDirection="row" 
-            columnGap={3} 
-            flexWrap="wrap" 
-            rowGap={3} 
-            justifyContent="space-between"
-        >
-            {ordersContext.ordersState.map((order) => {
-                // Set the status of the payment.
-                if(order.paid === true) {
-                    status = "Paid";
-                }
-                else {
-                    status = "Not Paid";
-                }
-                
-                if(order.served === true) {
-                    served = "Served";
-                }
-                else {
-                    served = "Not served";
-                }
+    if (isLoading) {
+        return (
+            <>
+                <LoadingSpinner />
+            </>
+        )
+    } else {
+        return (
+            <Box 
+                display="flex" 
+                flexDirection="row" 
+                columnGap={3} 
+                flexWrap="wrap" 
+                rowGap={3} 
+                justifyContent="space-between"
+            >
+                {orders.map((order) => {
+                    if(order.done === false) {
+                        served = "Not Served";
 
-                const orderDetails = extractOrders(order);
+                        if(order.paid === true) {
+                            status = "Paid";
+                        }
+                        else {
+                            status = "Not Paid";
+                        }
+                        const orderDetails = extractOrders(order.orders);
+            
+                        return (
+                            <Box key={order._id} component="div" flexBasis="32%">
+                                <Card sx={{ minWidth: 350 }} variant="outlined">
+                                    <CardContent>
+                                        {/* Todo: Add table number */}
+                                        <Typography variant="h6" component="div" alignSelf="center">
+                                            Order ID: {order._id}
+                                        </Typography>
+                                        <Typography variant="body1" color="text.secondary" fontWeight={"bold"}>
+                                            Paid Status: {status}
+                                        </Typography>
+                                        <Typography variant="body1" color="text.secondary" fontWeight={"bold"}>
+                                            Total Amount: RM {order.amount}
+                                        </Typography>
+                                        <Typography variant="body1" color="text.secondary" fontWeight={"bold"}>
+                                            Payment Method: {order.method.charAt(0).toUpperCase() + order.method.slice(1)}
+                                        </Typography>
+                                        <Typography variant="body1" color="text.secondary" fontWeight={"bold"}>
+                                            Time: {order.timestamp}
+                                        </Typography>
+                                        <Typography variant="body1" color="text.secondary" fontWeight={"bold"}>
+                                            Served: {served}
+                                        </Typography>
+                                    </CardContent>
+                                    <CardActions>
+                                        <OrderListDialog orderID={order._id} orderDetails={orderDetails}/>
+                                        <Button
+                                            onClick={() => handleFinish(order._id)}
+                                            size="small"
+                                            color="success"
+                                            variant="contained"
+                                            endIcon={<CheckCircleIcon />}
+                                        >
+                                            Finish
+                                        </Button>
+                                    </CardActions>
+                                </Card>
+                           </Box>
+                        );
+                    }
+                })}
+            </Box>
+        );
+    }
 
-                return (
-                    <Box component="div" flexBasis="32%">
-                        <Card sx={{ minWidth: 350 }} variant="outlined">
-                            <CardContent>
-                                {/* Todo: Add table number */}
-                                <Typography variant="h6" component="div" alignSelf="center">
-                                    Order ID: {order.id}
-                                </Typography>
-                                <Typography variant="body1" color="text.secondary">
-                                    Paid Status: {status}
-                                </Typography>
-                                <Typography variant="body1" color="text.secondary">
-                                    Total Amount: RM {order.amount}
-                                </Typography>
-                                <Typography variant="body1" color="text.secondary">
-                                    Payment Method: {order.payment}
-                                </Typography>
-                                <Typography variant="body1" color="text.secondary">
-                                    Served: {served}
-                                </Typography>
-                            </CardContent>
-                            <CardActions>
-                                <OrderListDialog orderDetails={orderDetails}/>
-                                <Button
-                                    onClick={() => handleFinish(order.id, order.served)}
-                                    size="small"
-                                    color="success"
-                                    variant="contained"
-                                    endIcon={<CheckCircleIcon />}
-                                >
-                                    Finish
-                                </Button>
-                            </CardActions>
-                        </Card>
-                   </Box>
-                );
-            })}
-        </Box>
-    );
 }
 
 export default OrderListCard;
